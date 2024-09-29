14a4ab288e4241eb8b950d8cfe7474112e732472
Krishilgithub <100427701+Krishilgithub@users.noreply.github.com>
2024-09-29 09:52:13 +0530

    made final ui

--- Project/Backend/public/js/main.js
+++ Project/Backend/public/js/main.js
@@ -1,7 +1,7 @@
 // Header Scroll
 let nav = document.querySelector(".navbar");
 window.onscroll = function() {
-    if (document.documentElement.scrollTop > 20) {
+    if (true) {
         nav.classList.add("header-scrolled");
     } else {
         nav.classList.remove("header-scrolled");
