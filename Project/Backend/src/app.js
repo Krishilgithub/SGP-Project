17f3e88ce4898d0f66f02bf17774b58ebdf88d80
Krishilgithub <100427701+Krishilgithub@users.noreply.github.com>
2025-04-08 22:11:00 +0530

    Configure project for Vercel deployment: Add vercel.json, modify app.js, and create api/index.js

--- Project/Backend/src/app.js
+++ Project/Backend/src/app.js
@@ -1,11 +1,10 @@
-const express = require('express');
-require('./db/conn');
-const Register = require('./models/register')
+const express = require("express");
+require("./db/conn");
+const Register = require("./models/register");
 const app = express();
-const hbs = require('hbs');
-const path = require('path');
-const bcrypt = require('bcryptjs')
-const PORT = process.env.PORT || 8000;
+const hbs = require("hbs");
+const path = require("path");
+const bcrypt = require("bcryptjs");
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
@@ -14,130 +13,125 @@ console.log(path.join(__dirname, "../templates/partials"));
 console.log(template_path);
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(static_path));
-app.set('views', template_path);
-app.set('view engine', 'hbs');
+app.set("views", template_path);
+app.set("view engine", "hbs");
 hbs.registerPartials(path.join(__dirname, "../templates/partials"));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/index', (req, res) => {
-    res.render('index');
+app.get("/index", (req, res) => {
+  res.render("index");
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/rooms', (req, res) => {
-    res.render('event');
+app.get("/rooms", (req, res) => {
+  res.render("event");
 });
 
-app.get('/register', (req, res) => {
-    res.render('register');
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard');
-})
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
 
-app.get('/complain', (req, res) => {
-    res.render('complain');
-})
+app.get("/complain", (req, res) => {
+  res.render("complain");
+});
 
-app.get('/alert', (req, res) => {
-    res.render('alert');
-})
+app.get("/alert", (req, res) => {
+  res.render("alert");
+});
 
-app.get('/room', (req, res) => {
-    res.render('room');
-})
+app.get("/room", (req, res) => {
+  res.render("room");
+});
 
-app.get('/menu', (req, res) => {
-    res.render('menu');
-})
+app.get("/menu", (req, res) => {
+  res.render("menu");
+});
 
-app.get('/fees', (req, res) => {
-    res.render('fees');
-})
+app.get("/fees", (req, res) => {
+  res.render("fees");
+});
 
-app.get('/gate', (req, res) => {
-    res.render('gate');
-})
+app.get("/gate", (req, res) => {
+  res.render("gate");
+});
 
-app.get('/notice', (req, res) => {
-    res.render('notice');
-})
+app.get("/notice", (req, res) => {
+  res.render("notice");
+});
 
-app.post('/register', async (req, res) => {
-    try {
+app.post("/register", async (req, res) => {
+  try {
+    const password = req.body.password;
+    const cpassword = req.body.confirmpassword;
 
-        const password = req.body.password;
-        const cpassword = req.body.confirmpassword;
+    if (cpassword === password) {
+      const registerEmployee = new Register({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        gender: req.body.gender,
+        phno: req.body.phone,
+        age: req.body.age,
+        password: req.body.password,
+        confirmPassword: req.body.confirmpassword,
+      });
 
-        if(cpassword === password){
-            const registerEmployee = new Register({
-                firstname : req.body.firstname,
-                lastname : req.body.lastname,
-                email : req.body.email,
-                gender : req.body.gender,
-                phno : req.body.phone,
-                age : req.body.age,
-                password : req.body.password,
-                confirmPassword : req.body.confirmpassword
-            });
-
-            const registered = await registerEmployee.save();
-            res.status(200).render('index');
-        }else{
-            res.send("Your password is not matching. Write password properly")
-        }
-
-    } catch (error) {
-        res.status(400).send(error);
+      const registered = await registerEmployee.save();
+      res.status(200).render("index");
+    } else {
+      res.send("Your password is not matching. Write password properly");
     }
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-app.post('/login', async (req, res) => {
-    try {
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
 
-        const email = req.body.email;
-        const password = req.body.password;
+    const username = await Register.findOne({ email: email });
+    const checkPassword = username.password;
 
-        const username = await Register.findOne({email:email});
-        const checkPassword = username.password;
-
-        if(!username){
-            res.send("Invalid login details");
-        }
-
-        // const isMatch = await bcrypt.compare(password, checkPassword);
-
-        if(checkPassword === password){
-            res.render('dashboard');}
-
-        // const check = await bcrypt.compare
-
-        // if(isMatch){
-        //     res.render('index');
-        // }
-        else{
-            res.send("Invalid login details");
-        }
-
-    } catch (error) {
-        res.send("There is some technical issue please try agian later")
+    if (!username) {
+      res.send("Invalid login details");
     }
-})
 
-app.listen(PORT, () => {
-    console.log("Listening to the port ", PORT);
+    // const isMatch = await bcrypt.compare(password, checkPassword);
+
+    if (checkPassword === password) {
+      res.render("dashboard");
+    }
+
+    // const check = await bcrypt.compare
+
+    // if(isMatch){
+    //     res.render('index');
+    // }
+    else {
+      res.send("Invalid login details");
+    }
+  } catch (error) {
+    res.send("There is some technical issue please try agian later");
+  }
 });
+
+module.exports = app;
---
+++ api/index.js
@@ -0,0 +1,3 @@
+const app = require("../Project/Backend/src/app");
+
+module.exports = app;
